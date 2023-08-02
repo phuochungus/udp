efac66b9878bc540605c26d467706113cd8ad75e
Nguyen Phuoc Hung <phuochungus@gmail.com>
2023-08-02 16:33:43 +0700

    send back message

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = UDP.createSocket('udp4')
 const port = process.env.PORT || 2222
 
 server.on('message', (message, info) => {
-    fs.writeFileSync('received.jpg', message)
+    server.send(message)
 })
 
 server.on('listening', () => {
